7dbddbd957e9e90a158be6c03aa3497036be04b4
João Carlos <doidobr46@gmail.com>
2023-11-28 14:16:59 -0300

    Current time printing util added.

--- src/main.cc
+++ src/main.cc
@@ -10,6 +10,7 @@
 #include "config.h"
 #include "types.h"
 #include "utils/network_utils.h"
+#include "utils/time_utils.h"
 
 using namespace std;
 
@@ -87,35 +88,15 @@ int main(int argc, char* argv[]) {
             "Failed to send operation datagram.\n"
         );
         
-        // Get the current time
-        time_t send_time;
-        time(&send_time);
-
-        // Convert the time to a tm struct for formatting
-        struct tm* send_time_tm = localtime(&send_time);
-
-        // Format and print the current time
-        char send_time_buffer[80];
-        strftime(send_time_buffer, sizeof(send_time_buffer), "%Y-%m-%d %H:%M:%S", send_time_tm);
-        printf("Send time: %s\n", send_time_buffer);
-
-        // check(
-        //     (recvfrom(client_socket, &ack_operation_packet, sizeof(operation_datagram_t), 0, (struct sockaddr*)&server_addr, &server_addrLen)),
-        //     "Failed to receive ack operation datagram.\n"
-        // );
-        // printf("Mensagem de ack recebida.\n");
-        recvfrom(client_socket, &ack_operation_packet, sizeof(operation_datagram_t), 0, (struct sockaddr*)&server_addr, &server_addrLen);
-        // Get the current time
-        time_t current_time;
-        time(&current_time);
-
-        // Convert the time to a tm struct for formatting
-        struct tm* current_time_tm = localtime(&current_time);
-
-        // Format and print the current time
-        char current_time_buffer[80];
-        strftime(current_time_buffer, sizeof(current_time_buffer), "%Y-%m-%d %H:%M:%S", current_time_tm);
-        printf("Current time: %s\n", current_time_buffer);
+        while ( 0 > recvfrom(client_socket, &ack_operation_packet, sizeof(operation_datagram_t), 0, (struct sockaddr*)&server_addr, &server_addrLen))
+        {
+            printf("Failed to receive ack from server, retrying connection...\n");
+            printCurrentTime();
+            check(
+                (sendto(client_socket, &operation_packet, sizeof(operation_datagram_t), 0, (struct sockaddr*)&server_addr, sizeof(server_addr))),
+                "Failed to send operation datagram.\n"
+            );
+        }
 
         if (operation == "upload") {
             uploadFile(file_path.c_str());
