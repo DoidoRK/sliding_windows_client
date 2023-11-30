e7071546f0e24a32c95047e7b36dc63aff6e4880
João Carlos <doidobr46@gmail.com>
2023-11-30 18:44:05 -0300

    Multithread implementation started

--- src/config.h
+++ src/config.h
@@ -3,19 +3,24 @@
 
 //Connection settings
 #define SERVER_PORT 4000
+
 #define SERVER_IP "192.168.171.67"  // Server's IP Address
 
 //Socket timeout settings
-#define SOCKET_TIMEOUT_IN_SECONDS 0
-#define SOCKET_TIMEOUT_IN_MICROSSECONDS 10000
+#define SOCKET_TIMEOUT_IN_SECONDS 2
+#define SOCKET_TIMEOUT_IN_MICROSSECONDS 0
+//If time out time is lesser than
+// WINDOW_SIZE * DELAY_BETWEEN_THREAD_CREATION
+//Unexpected behaviour may appear
 
 //Error simulation settings
 #define TIMEOUT_CHANCE_PERCENT  70
 
 //Sliding Windows settings
 #define FILE_NAME_SIZE 128
-#define WINDOW_SIZE 1
+#define WINDOW_SIZE 5
 #define CHUNK_SIZE 128 //In Bytes
+#define DELAY_BETWEEN_THREAD_CREATION 10
 
 #define FILE_PATH "/home/doidobr/Projetos/Redes/sliding_windows_client/src/files/"
 
--- src/sliding_windows.h
+++ src/sliding_windows.h
@@ -15,20 +15,22 @@
 #include "utils/network_utils.h"
 #include "utils/time_utils.h"
 #include "utils/file_utils.h"
+#include "utils/print_utils.h"
 
-pthread_mutex_t sliding_window_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t current_chunk_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t window_end_chunk_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t frame_list_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_t sliding_window[WINDOW_SIZE];
 
 frame_t *frame_list = NULL;
 int is_running = 0;
-size_t number_of_chunks = 0;
-size_t current_chunk = 0;
+size_t number_of_chunks_in_file = 0;
+size_t current_chunk = 0, window_end_chunk = 0;
 
 using namespace std;
 
-void loadFramesFromFile(const char file_path[FILE_NAME_SIZE], size_t number_of_chunks) {
+void loadFramesFromFile(const char file_path[FILE_NAME_SIZE], size_t number_of_chunks_in_file) {
     FILE *file = fopen(file_path, "rb"); // Open the file in binary mode for reading
     if (file == NULL) {
         perror("Error opening file");
@@ -36,7 +38,7 @@ void loadFramesFromFile(const char file_path[FILE_NAME_SIZE], size_t number_of_c
     }
 
     // Dynamically allocate memory for the frame list
-    frame_list = (frame_t *)malloc(number_of_chunks * sizeof(frame_t));
+    frame_list = (frame_t *)malloc(number_of_chunks_in_file * sizeof(frame_t));
     if (frame_list == NULL) {
         perror("Memory allocation failed");
         fclose(file);
@@ -44,7 +46,7 @@ void loadFramesFromFile(const char file_path[FILE_NAME_SIZE], size_t number_of_c
     }
 
     // Read frames from the file into the dynamically allocated frame list
-    for (size_t i = 0; i < number_of_chunks; ++i) {
+    for (size_t i = 0; i < number_of_chunks_in_file; ++i) {
         frame_list[i].status = NOT_ACKNOWLEDGED; // Start frames as not acknowledged
 
         // Read data from the file into the data property
@@ -61,6 +63,7 @@ void loadFramesFromFile(const char file_path[FILE_NAME_SIZE], size_t number_of_c
     fclose(file); // Close the file after reading
 }
 
+//Upload proccess
 void* uploadFileThread(void* arg){
     uint16_t thread_port = *((uint16_t*)arg);
     int thread_socket;
@@ -94,54 +97,66 @@ void* uploadFileThread(void* arg){
     FD_ZERO(&readSet);
     FD_SET(thread_socket, &readSet);
 
-    while (current_chunk < number_of_chunks)
+    while (current_chunk < number_of_chunks_in_file)
     {
-        data_packet.sequence_number = current_chunk;
-        memcpy(&(data_packet.frame), &(frame_list[current_chunk]), sizeof(frame_t));
-
-        cout << "Frame number: " << current_chunk << " readed from frame list!" << endl;
-        cout << "Thread Port: " << thread_port << " sending data packet!" << endl;
-        cout << "Packet info: ";
-        cout << "Status: " << (data_packet.frame.status? "ACKNOWLEDGED" : "NOT_ACKNOWLEDGED");
-        cout << " Sequence Number: " << data_packet.sequence_number << endl << endl;
-        check(
-            (sendto(thread_socket, &data_packet, sizeof(data_packet_t), 0, (struct sockaddr*)&server_addr, sizeof(server_addr))),
-            "Upload thread failed to send data packet.\n"
-        );
-        // Data is ready to be received
-        int recv_result = recvfrom(thread_socket, &ack_packet, sizeof(data_packet_t), 0, (struct sockaddr*)&server_addr, &server_addr_len);
-        if (recv_result < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                // Timeout occurred
-                cout << "Ack packet for sequence number: "<< current_chunk << " Timeout occurred!" << endl;
-            } else {
-                perror("Error receiving data");
-            }
-        } else {
-            cout << "Thread Port: " << thread_port << " received ack packet!" << endl;
-            cout << "Packet info: ";
-            cout << "Status: " << (ack_packet.frame.status? "ACKNOWLEDGED" : "NOT_ACKNOWLEDGED");
-            cout << " Sequence Number: " << ack_packet.sequence_number << endl << endl;
-            if(ACKNOWLEDGED == ack_packet.frame.status){
-                frame_list[current_chunk].status = ACKNOWLEDGED;
-                current_chunk++;
+        if(window_end_chunk - current_chunk < WINDOW_SIZE) {
+            if(window_end_chunk < number_of_chunks_in_file){
+                data_packet.sequence_number = window_end_chunk;
+                pthread_mutex_lock(&frame_list_mutex);
+                memcpy(&(data_packet.frame), &(frame_list[window_end_chunk]), sizeof(frame_t));
+                pthread_mutex_lock(&window_end_chunk_mutex);
+                window_end_chunk++;
+                pthread_mutex_unlock(&window_end_chunk_mutex);
+                pthread_mutex_unlock(&frame_list_mutex);
+                if(NOT_ACKNOWLEDGED == data_packet.frame.status){
+                    printDataPacket(current_chunk, number_of_chunks_in_file, thread_port, data_packet, SEND_DATA_PACKET);
+                    check(
+                        (sendto(thread_socket, &data_packet, sizeof(data_packet_t), 0, (struct sockaddr*)&server_addr, sizeof(server_addr))),
+                        "Upload thread failed to send data packet.\n"
+                    );
+                    // Data is ready to be received
+                    int recv_result = recvfrom(thread_socket, &ack_packet, sizeof(data_packet_t), 0, (struct sockaddr*)&server_addr, &server_addr_len);
+                    if (recv_result < 0) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            // Timeout occurred
+                            printTimeOutError(data_packet.sequence_number);
+                        } else {
+                            perror("Error receiving data");
+                        }
+                        pthread_mutex_lock(&window_end_chunk_mutex);
+                        //If there is a error receiving package ack, go back N
+                        window_end_chunk = current_chunk;
+                        pthread_mutex_unlock(&window_end_chunk_mutex);
+                    } else {
+                        printDataPacket(current_chunk,number_of_chunks_in_file, thread_port, ack_packet, RECV_DATA_PACKET);
+                        if(ACKNOWLEDGED == ack_packet.frame.status){
+                            pthread_mutex_lock(&frame_list_mutex);
+                            frame_list[window_end_chunk].status = ACKNOWLEDGED;
+                            pthread_mutex_lock(&current_chunk_mutex);
+                            current_chunk++;
+                            pthread_mutex_unlock(&current_chunk_mutex);
+                            pthread_mutex_unlock(&frame_list_mutex);
+                        }
+                    }
+                } else {
+                    pthread_mutex_lock(&current_chunk_mutex);
+                    current_chunk++;
+                    pthread_mutex_unlock(&current_chunk_mutex);
+                }
             }
         }
-        // pthread_mutex_lock(&current_chunk_mutex);
-        // pthread_mutex_unlock(&current_chunk_mutex);
     }
     is_running = 0;
     return 0;
 }
 
-//Upload proccess
 void uploadFile(string file_name){
     is_running = 1;
     string file_path = FILE_PATH + file_name;
     cout << file_path << endl;
-    number_of_chunks = calculateNumChunks(file_path.c_str(), CHUNK_SIZE);
-    cout << "Quantidade de Chunks " << number_of_chunks << endl;
-    loadFramesFromFile(file_path.c_str(), number_of_chunks);
+    number_of_chunks_in_file = calculateNumChunks(file_path.c_str(), CHUNK_SIZE);
+    cout << "Quantidade de Chunks " << number_of_chunks_in_file << endl;
+    loadFramesFromFile(file_path.c_str(), number_of_chunks_in_file);
     cout << "Carregou arquivo no Buffer" << endl;
 
     //Starts upload to server
@@ -152,7 +167,7 @@ void uploadFile(string file_name){
 
     operation_packet.ftp_mode = UPLOAD;
     strcpy(operation_packet.file_name,file_name.c_str());
-    operation_packet.number_of_chunks = number_of_chunks;
+    operation_packet.number_of_chunks_in_file = number_of_chunks_in_file;
 
     check(
         (client_socket = socket(AF_INET, SOCK_DGRAM, 0)),
@@ -167,7 +182,7 @@ void uploadFile(string file_name){
         "Failed to set server address.\n"
     );
 
-    struct timeval timeout;      
+    struct timeval timeout;
     timeout.tv_sec = SOCKET_TIMEOUT_IN_SECONDS;
     timeout.tv_usec = SOCKET_TIMEOUT_IN_MICROSSECONDS;
 
@@ -191,15 +206,15 @@ void uploadFile(string file_name){
         );
     }
 
-    //Creates server threads to handle data packets.
-    for (uint8_t i = 0; i < WINDOW_SIZE; i++)
+    //Creates server packets to handle data packets
+    for (size_t i = 0; i < WINDOW_SIZE; i++)
     {
-        uint16_t thread_port = SERVER_PORT+(i+1);
-        cout << "Assigned thread port: " << thread_port << endl;
-         if (pthread_create(&sliding_window[i], NULL, uploadFileThread, (void*)&thread_port) != 0) {
+        uint16_t thread_port = SERVER_PORT + 1 + i;
+        if (pthread_create(&sliding_window[i], NULL, uploadFileThread, (void*)&thread_port) != 0) {
             perror("Error creating download thread.\n");
             return;
         }
+        usleep(DELAY_BETWEEN_THREAD_CREATION);
     }
     while (is_running); 
     close(client_socket);
@@ -207,13 +222,6 @@ void uploadFile(string file_name){
 
 
 //Download proccess
-
-//Creates sliding windows threads to deal with file transfer.
-// for (uint8_t i = 0; i < WINDOW_SIZE; i++)
-// {
-//     pthread_create(&sliding_window[i],NULL, fileDownloadThread, NULL);
-// }
-
 void downloadFile(const char *file_name){
     cout << "Fazendo Download de: " << file_name << endl;
 }
